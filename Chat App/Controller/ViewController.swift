5f8089970d86d85f0a5c66f2f1e598c71e175d78
Tprezioso <tommyprezioso@gmail.com>
2018-04-16 17:10:48 -0400

    first commit

---
+++ Chat App/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chat App
+//
+//  Created by Thomas Prezioso on 4/16/18.
+//  Copyright © 2018 Thomas Prezioso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
