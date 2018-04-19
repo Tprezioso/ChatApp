04c35953142407b5e0f9a573964f88ba7c25777b
Tprezioso <tommyprezioso@gmail.com>
2018-04-19 16:27:10 -0400

    added firebase to the project

--- Chat App/Controller/LoginViewController.swift
+++ Chat App/Controller/LoginViewController.swift
@@ -7,20 +7,45 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
+    @IBOutlet var nameField: UITextField!
+    // MARK: View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.blue
-        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+    
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShowNotification(_:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+//        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHideNotification(_:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+//    }
+//    
+//    override func viewWillDisappear(_ animated: Bool) {
+//        super.viewWillDisappear(animated)
+//        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+//        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+//    }
+
+
+    @IBAction func loginDidTouch(_ sender: Any) {
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+//    // MARK: - Notifications
+//
+//    @objc func keyboardWillShowNotification(_ notification: Notification) {
+//        let keyboardEndFrame = ((notification as NSNotification).userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+//        let convertedKeyboardEndFrame = view.convert(keyboardEndFrame, from: view.window)
+//        bottomLayoutGuideConstraint.constant = view.bounds.maxY - convertedKeyboardEndFrame.minY
+//    }
+//
+//    @objc func keyboardWillHideNotification(_ notification: Notification) {
+//        bottomLayoutGuideConstraint.constant = 48
+//    }
 
 }
 
