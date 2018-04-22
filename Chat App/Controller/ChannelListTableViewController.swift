40a244c655c4ff407b416f8f038d63be11de2f80
Tprezioso <tommyprezioso@gmail.com>
2018-04-22 15:55:35 -0400

    testing that setup of the tableview controller works

--- Chat App/Controller/ChannelListTableViewController.swift
+++ Chat App/Controller/ChannelListTableViewController.swift
@@ -19,7 +19,14 @@ class ChannelListTableViewController: UITableViewController {
     var newChannelTextField: UITextField?
     private var channels: [Channel] = []
     
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        channels.append(Channel(id: "1", name: "Channel1"))
+        channels.append(Channel(id: "2", name: "Channel2"))
+        channels.append(Channel(id: "3", name: "Channel3"))
+        self.tableView.reloadData()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -37,22 +44,36 @@ class ChannelListTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 10
+        if let currentSection: Section = Section(rawValue: section) {
+            switch currentSection {
+            case .createNewChannelSection:
+                return 1
+            case .currentChannelsSection:
+                return channels.count
+            }
+        } else {
+            return 0
+        }
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.row == 0 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "NewChannel", for: indexPath)
-            return cell
-        } else {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "ExistingChannel", for: indexPath)
-            return cell
+        let reuseIdentifier = (indexPath as NSIndexPath).section == Section.createNewChannelSection.rawValue ? "NewChannel" : "ExistingChannel"
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        
+        if (indexPath as NSIndexPath).section == Section.createNewChannelSection.rawValue {
+            if let createNewChannelCell = cell as? CreateChannelCell {
+                newChannelTextField = createNewChannelCell.newChannelNameField
+            }
+        } else if (indexPath as NSIndexPath).section == Section.currentChannelsSection.rawValue {
+            cell.textLabel?.text = channels[(indexPath as NSIndexPath).row].name
         }
-    }
+        
+        return cell    }
 
     /*
     // Override to support conditional editing of the table view.
